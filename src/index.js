326492713ce25d2f6235ea433d856ceb4b9d7696
averikaa <averika16@gmail.com>
2023-01-15 00:21:37 +0100

    feat: made task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let numStr = Math.abs(n).toString();
+  let completeStr = "";
+  for(let i = numStr.length - 1; i >= 0; i--){
+    completeStr = completeStr + numStr[i];
+  }
+  return completeStr;
 }
